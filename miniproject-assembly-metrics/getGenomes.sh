601bcecb667348f4578af305ad5fe5f73a7dd74f
nalagna1 <nalagna1@jh.edu>
2025-09-07 21:27:01 -0400

    Add files via upload

---
+++ miniproject-assembly-metrics/getGenomes.sh
@@ -0,0 +1,11 @@
+
+# 3. Create a Bash script to download the genome assemblies
+
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA248909/caenorhabditis_remanei.PRJNA248909.WBPS19.genomic.fa.gz
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA248911/caenorhabditis_remanei.PRJNA248911.WBPS19.genomic.fa.gz
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA53967/caenorhabditis_remanei.PRJNA53967.WBPS19.genomic.fa.gz
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA577507/caenorhabditis_remanei.PRJNA577507.WBPS19.genomic.fa.gz
+
+# Make the file executable using chmod +x
+
+# Run the script using ./getGenomes.sh
